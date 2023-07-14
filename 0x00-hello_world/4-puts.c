2cd0efd61ccde7a14520250cb0803fed01fb1e63
felixkiondo <feshkiondo@gmail.com>
2023-07-14 07:34:37 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 # include <stdio.h>
 /**
- *main - entry poin
- *return: always 0 (success)
+ *main - entry point
+ *
+ *Return: always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
