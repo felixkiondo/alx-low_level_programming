75870fa8eb2bded7392b42226f3b6fe754dc1431
felixkiondo <feshkiondo@gmail.com>
2023-07-17 12:59:38 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print base 16 numbers'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (n = 97; n <= 102; n++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
