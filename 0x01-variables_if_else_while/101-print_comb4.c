f4de39011900176ac0005922188ab6831946fd4e
felixkiondo <feshkiondo@gmail.com>
2023-10-05 08:19:54 -0700

    a program that prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,7 +17,7 @@ int main(void)
 				putchar(i);
 				putchar(j);
 				putchar(k);
-				if (i != 55 || j != 57 || k != 57)
+				if (i != 55 || j != 56)
 				{
 					putchar(',');
 					putchar(' ');
