9195852b26b73af7c47671b97ee8cf2705977f96
felixkiondo <feshkiondo@gmail.com>
2023-10-05 06:01:29 -0700

    a program that rints lowercase letters in reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Description: 'print lowecase in reverse'
- * Return: Always 0 (Success)
- */
+ * main - prints lower alphabet letters in reverse
+ * followed by a new line
+ * Return: Always (0) success
+*/
 int main(void)
 {
-	int n = 97;
+	int a, b;
 
-	{
-		while (n <= 122)
-			putchar(n);
-			n--;
-	}
-	putchar('\n');
+	for (a = 122; a >= 97; a--)
+		putchar (a);
+
 	return (0);
 }
